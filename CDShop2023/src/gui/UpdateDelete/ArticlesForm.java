35d3cdc8e622d949430d28866a3a379c763a6c02
ljubicamiljkovic56 <ljubica.m19@gmail.com>
2023-02-11 22:01:03 +0100

    Working on ArticlesForm class

--- CDShop2023/src/gui/UpdateDelete/ArticlesForm.java
+++ CDShop2023/src/gui/UpdateDelete/ArticlesForm.java
@@ -1,9 +1,56 @@
 package gui.UpdateDelete;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+import articles.Article;
+import shop.Shop;
 
 //class for editing articles form
 @SuppressWarnings("serial")
 public class ArticlesForm extends JFrame {
+	
+	private JLabel lblIdCode = new JLabel("ID Code");
+	private JTextField txtIdCode = new JTextField(20);
+	private JLabel lblPublisher = new JLabel("Publisher");
+	private JTextField txtPublisher = new JTextField(20);
+	private JLabel lblYear = new JLabel("Year of Release");
+	private JTextField txtYear = new JTextField(20);
+	private JLabel lblPrice = new JLabel("Price");
+	private JTextField txtPrice = new JTextField(20);
+	private JLabel lblAvailable = new JLabel("Available Copies");
+	private JTextField txtAvailable = new JTextField(20);
+	private JLabel lblSoldOut = new JLabel("Sold Out Copies");
+	private JTextField txtSoldOut = new JTextField(20);
+	private JLabel lblName = new JLabel("Name");
+	private JTextField txtName = new JTextField(20);
+	
+	private Shop shop;
+	private Article article;
+	
+	public ArticlesForm(Shop shop, Article article) {
+		this.shop = shop;
+		this.article = article;
+		if(article != null) {
+			setTitle("Edit this article - " + article.getName());
+		}else {
+			setTitle("Add new article");
+		}
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setLocationRelativeTo(null);
+		setResizable(false);
+		initGUI();
+		initActions();
+		pack();
+	}
+	
+	private void initGUI() {
+		
+	}
+	
+	private void initActions() {
+		
+	}
 
 }
